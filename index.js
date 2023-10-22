154f516493d4db5f12edb4eba60515db105d5dfa
azzizulhakim810 <azzizul.hakim202@gmail.com>
2023-10-22 23:38:49 +0600

    post method implementation

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-console.log(process.env.SECRET_KEY);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECRET_KEY}@atlascluster.sztfigr.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -28,12 +26,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db('productDB').collection('user');
+
+    
+
+    app.post('/product', async(req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await userCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+    // User related api's 
+    app.post('/user', (req, res) => {
+
+    })
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
